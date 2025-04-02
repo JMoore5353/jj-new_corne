d6382cb37e5579b1da122e23c968308a852745e2
Jacob Moore <jacobmoor2@gmail.com>
2025-04-02 13:31:03 -0600

    colemakdh firmware

--- config/miryoku/miryoku/custom_config.h
+++ config/miryoku/miryoku/custom_config.h
@@ -2,7 +2,7 @@
 // https://github.com/manna-harbour/miryoku
 
 // Define keyboard layers
-#define MIRYOKU_ALPHAS_QWERTY
+#define MIRYOKU_ALPHAS_COLEMAKDH
 #define MIRYOKU_EXTRA_QWERTY  // Defaults to QWERTY if not defined
 #define MIRYOKU_TAP_QWERTY
 #define MIRYOKU_NAV_VI        // This define takes care of the Media and Mouse layers as well
