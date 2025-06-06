8e76a2aee15506a92ab773e38739ec471e2aef71
Jacob Moore <jacobmoor2@gmail.com>
2025-06-06 14:52:09 -0600

    bug: removed submodule since I want to freeze miryoku version with pointer encoder to this branch

---
+++ config/miryoku_zmk/miryoku/custom_config.h
@@ -0,0 +1,12 @@
+// Copyright 2021 Manna Harbour
+// https://github.com/manna-harbour/miryoku
+
+// Define keyboard layers
+#define MIRYOKU_ALPHAS_QWERTY
+#define MIRYOKU_EXTRA_QWERTY  // Defaults to QWERTY if not defined
+#define MIRYOKU_TAP_QWERTY
+#define MIRYOKU_NAV_VI        // This define takes care of the Media and Mouse layers as well
+
+// Enable pointing
+#define MIRYOKU_MOUSEKEYS_POINTING
+
